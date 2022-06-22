77213b70663625311f5c7361474105403590d594
irawan19 <irawan_agungnugroho@yahoo.com>
2022-06-22 21:13:23 +0700

    update swagger

--- resources/views/vendor/l5-swagger/index.blade.php
+++ resources/views/vendor/l5-swagger/index.blade.php
@@ -33,35 +33,40 @@
 <script src="{{ l5_swagger_asset($documentation, 'swagger-ui-bundle.js') }}"></script>
 <script src="{{ l5_swagger_asset($documentation, 'swagger-ui-standalone-preset.js') }}"></script>
 <script>
-window.onload = function() {
-  // Build a system
-  const ui = SwaggerUIBundle({
-    dom_id: '#swagger-ui',
+    window.onload = function() {
+        // Build a system
+        const ui = SwaggerUIBundle({
+            dom_id: '#swagger-ui',
+            url: "{!! $urlToDocs !!}",
+            operationsSorter: {!! isset($operationsSorter) ? '"' . $operationsSorter . '"' : 'null' !!},
+            configUrl: {!! isset($configUrl) ? '"' . $configUrl . '"' : 'null' !!},
+            validatorUrl: {!! isset($validatorUrl) ? '"' . $validatorUrl . '"' : 'null' !!},
+            oauth2RedirectUrl: "{{ route('l5-swagger.'.$documentation.'.oauth2_callback', [], $useAbsolutePath) }}",
 
-    url: "{!! $urlToDocs !!}",
-    operationsSorter: {!! isset($operationsSorter) ? '"' . $operationsSorter . '"' : 'null' !!},
-    configUrl: {!! isset($configUrl) ? '"' . $configUrl . '"' : 'null' !!},
-    validatorUrl: {!! isset($validatorUrl) ? '"' . $validatorUrl . '"' : 'null' !!},
+            requestInterceptor: function(request) {
+                request.headers['X-CSRF-TOKEN'] = '{{ csrf_token() }}';
+                return request;
+            },
 
-    requestInterceptor: function(req) {
-      req.headers['X-CSRF-TOKEN'] = '{{ csrf_token() }}';
-      return req;
-    },
+            presets: [
+                SwaggerUIBundle.presets.apis,
+                SwaggerUIStandalonePreset
+            ],
 
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
+            plugins: [
+                SwaggerUIBundle.plugins.DownloadUrl
+            ],
 
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
+            layout: "StandaloneLayout",
+            docExpansion : "{!! config('l5-swagger.defaults.ui.display.doc_expansion', 'none') !!}",
+            deepLinking: true,
+            filter: {!! config('l5-swagger.defaults.ui.display.filter') ? 'true' : 'false' !!},
+            persistAuthorization: "{!! config('l5-swagger.defaults.ui.authorization.persist_authorization') ? 'true' : 'false' !!}",
 
-    layout: "StandaloneLayout"
-  })
+        })
 
-  window.ui = ui
-}
+        window.ui = ui
+    }
 </script>
 </body>
 </html>
